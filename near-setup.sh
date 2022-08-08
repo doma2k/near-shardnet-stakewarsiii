f36dece2865038a7db05e179f046c74185fd9b1a
ImStaked <82274336+ImStaked@users.noreply.github.com>
2022-08-08 12:19:48 -0500

    Update near-setup.sh

--- near-setup.sh
+++ near-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+su near
 source $HOME/.cargo/env
 git clone https://github.com/near/nearcore
 cd nearcore
@@ -15,23 +16,4 @@ echo 'export NEAR_ENV=shardnet' >> ~/.bashrc
 echo 'export NEAR_ENV=shardnet' >> ~/.bash_profile
 source $HOME/.bash_profile
     
-file_location="/etc/systemd/system/neard.service"
-cat > $file_location <<EOF
-  [Unit]
-  Description=NEARd Daemon Service
 
-  [Service]
-  Type=simple
-  User=near
-  Group=near
-  WorkingDirectory=/home/near/.near
-  ExecStart=neard run --home /home/near/.near
-  Restart=on-failure
-  RestartSec=30
-  KillSignal=SIGINT
-  TimeoutStopSec=45
-  KillMode=mixed
-
-  [Install]
-  WantedBy=multi-user.target   
-EOF
